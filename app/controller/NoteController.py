b42b8e639e9faac95a5aae2533b4d1567c88c9f4
陈志虎 <Tora_Chen@outlook.com>
2025-03-22 11:16:20 +0800

    添加Note相关端点

--- app/controller/NoteController.py
+++ app/controller/NoteController.py
@@ -1,6 +1,6 @@
 # 这个文件用来存放Note相关的控制器
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlmodel import select
 
 from app.entity import Note
@@ -13,8 +13,21 @@ router = APIRouter(
     dependencies=[Depends(has_role("ROLE_STUDENT"))]
 )
 
-@router.get("/certainNote/{note_id}")
+@router.get("/certainNote/{note_id}", deprecated=True) # 根据笔记ID查找笔记（弃用）
 def find_certain_note(
+    note_id: int,
+    session: SessionDep,
+    response: Response
+) -> Note:
+    # 由于不符合API命名规范，弃用该端点，使用 /api/notes/{note_id} 代替
+    response.headers["X-API-Warning"] = "This endpoint is deprecated. Use /api/notes/{note_id} instead."
+    note = session.get(Note, note_id)
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return note
+
+@router.get("/{note_id}") # 根据笔记ID查找笔记
+def get_note(
     note_id: int,
     session: SessionDep,
 ) -> Note:
@@ -23,7 +36,7 @@ def find_certain_note(
         raise HTTPException(status_code=404, detail="Note not found")
     return note
 
-@router.get("/videoNotes/{video_id}")
+@router.get("/videoNotes/{video_id}") # 根据视频ID查找该视频下的所有笔记
 def find_video_notes(
     video_id: int,
     session: SessionDep,
@@ -32,3 +45,13 @@ def find_video_notes(
         select(Note).where(Note.video_id == video_id)
     ).all()
     return notes
+
+@router.post("/") # 创建一个新的笔记
+def add_note(
+    session: SessionDep,
+    note: Note
+) -> Note:
+    session.add(note)
+    session.commit()
+    session.refresh(note)
+    return note
